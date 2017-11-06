bc6ab67602200859b449c06fe56fe3da44fc78cc
Roberta Renally da Silva Melo <robertarenallydsm@gmail.com>
2017-11-06 00:34:04 -0200

    implementação da tela de nova enquete

---
+++ app/src/main/java/com/forum/forum/nova_enquete.java
@@ -0,0 +1,13 @@
+package com.forum.forum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class nova_enquete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nova_enquete);
+    }
+}
