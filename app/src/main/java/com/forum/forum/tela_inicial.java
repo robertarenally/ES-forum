ce06ba145e142526a611a6a5cc4f192b88a6ebe1
Roberta Renally da Silva Melo <robertarenallydsm@gmail.com>
2017-11-05 23:39:35 -0200

    Implementação de algumas telas

---
+++ app/src/main/java/com/forum/forum/tela_inicial.java
@@ -0,0 +1,13 @@
+package com.forum.forum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tela_inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
