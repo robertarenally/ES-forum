94a89e3137940b2d1c7285ab59615db70b52793f
Roberta Renally da Silva Melo <robertarenallydsm@gmail.com>
2017-11-07 14:12:58 -0200

    Ultimas telas

---
+++ app/src/main/java/com/forum/forum/editar_usuario.java
@@ -0,0 +1,13 @@
+package com.forum.forum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editar_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editar_usuario);
+    }
+}
